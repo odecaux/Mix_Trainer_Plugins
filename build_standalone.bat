acfd7e75d6828f03349498c464ec9a4d893dd8ed
Octave Decaux <odecaux@gmail.com>
2022-12-03 15:50:29 +0100

    v8 !

--- build_standalone.bat
+++ build_standalone.bat
@@ -1,1 +1,1 @@
-pwsh -Command "$dir = (Resolve-Path .\).tostring(); $dir = $dir + '\'; echo $dir; (cmake --build cmake-build --target Standalone) | foreach-object{ $_.replace($dir,'')} | foreach-object{ $_.replace($dir,'')}"
+pwsh -Command "$dir = (Resolve-Path .\).tostring(); $dir = $dir + '\'; echo $dir; (cmake --build cmake-build --target MixTrainer_Standalone) | foreach-object{ $_.replace($dir,'')} | foreach-object{ $_.replace($dir,'')}"
